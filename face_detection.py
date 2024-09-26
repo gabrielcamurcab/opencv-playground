52a4eda6c43253002302c50d7ae9523dd9a2e54e
Gabriel Camurça <projetosdocamurca@gmail.com>
2024-09-26 16:00:24 -0300

    add face detector file

---
+++ face_detection.py
@@ -0,0 +1,26 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(2)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255,0,0), 2)
+
+    cv2.imshow('FaceDetector', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
